17df149abda5ccf28c1d1c67f47afa5b581719af
insertdead <mdsc0111@tuta.io>
2021-04-01 10:40:13 -0400

    wow turns out indentation is pretty important in python

--- flair-client/api.py
+++ flair-client/api.py
@@ -23,33 +23,33 @@ class Auth:
         self.credentials = credentials
         self.api_root = api_root
 
-        async def create_url(self, path):
-            return urljoin(self.api_root, path)
+    async def create_url(self, path):
+        return urljoin(self.api_root, path)
 
-        async def oauth_token(self):
-            params = dict(
-                client_id=self.credentials["client_id"],
-                client_secret=self.credentials["client_secret"],
-                grant_type="client_credentials",
-            )
-            async with ClientSession as session:
-                async with session.post(
-                    self.create_url("/oauth/token"), params=params
-                ) as resp:
-                    self._token = resp.json().get("access_token") # For both of these - I'm not sure whether or not await is necessary
-                    self._expires_in = resp.json().get("expires_in")
-                    return resp.status
+    async def oauth_token(self):
+        params = dict(
+            client_id=self.credentials["client_id"],
+            client_secret=self.credentials["client_secret"],
+            grant_type="client_credentials",
+        )
+        async with ClientSession as session:
+            async with session.post(
+                self.create_url("/oauth/token"), params=params
+            ) as resp:
+                self._token = resp.json().get("access_token") # For both of these - I'm not sure whether or not await is necessary
+                self._expires_in = resp.json().get("expires_in")
+                return await resp.status
 
-# class Control:
+# class Control():
 #
-#     """Get status and control Flair products with API"""
+#     """Control and get statuses of flair products accessible by the API"""
 #
-#     def __init__(self, api=DEFAULT_API_ROOT):
-#         """TODO
+#     def __init__(self):
+#         """TODO: asyncio is hard :( """
+#         pass
 #
-#         :api: TODO
+#     async def set(self, name, **kwargs):
+#         params = kwargs.get('params')
 #
-#         """
-#         api.__init__(self)
-#
-#         self._api = api
+#         async with Auth as auth:
+#             await auth.oauth_token
