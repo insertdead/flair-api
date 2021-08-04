567cef838eef54e92978b803b1cc0f9a8a930749
insertdeadmeme <mdsc0111@tuta.io>
2021-08-04 15:43:51 -0400

    remove tests dir and improve use of __init__.py

--- flair_api/__init__.py
+++ flair_api/__init__.py
@@ -1,2 +1,10 @@
 __version__ = "0.1.0"
-__all__ = ["Client"]
+
+from .flair_client import (
+    Get as Get,
+    Control as Control,
+    Client as Client,
+    Utilities as Utilities
+)
+
+__all__ = ['Client', 'Get']
